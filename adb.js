5a0bb593f1723adda97f59c8ab280dd3f5e680ab
Ricky Miller <ricky.miller@gmail.com>
2017-06-17 11:08:20 -0400

    Use async await

--- adb.js
+++ adb.js
@@ -5,40 +5,40 @@ const adb = { tasks: {} }
 const noop = () => {}
 
 adb.push = (opts) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     opts = opts || {}
     opts.args = opts.args || [ '/dev/null', '/sdcard/' ]
     opts.onexit = opts.onexit || noop
-    uid(20).then(uid => {
-      const push = adb.tasks[uid] = spawn(cmd, [ 'push', '-p', ...opts.args ])
-      push.stderr.on('data', opts.ondata || noop)
-      push.stdout.on('data', opts.ondata || noop)
-      push.on('exit', (code) => {
-        opts.onexit(code)
-        delete adb.tasks[uid]
-      })
-      resolve(uid)
+    const id = await uid(20)
+    const push = adb.tasks[id] = spawn(cmd, [ 'push', '-p', ...opts.args ])
+    push.stderr.on('data', opts.ondata || noop)
+    push.stdout.on('data', opts.ondata || noop)
+    push.on('exit', (code) => {
+      opts.onexit(code)
+      delete adb.tasks[id]
     })
+    resolve(id)
   })
 }
 
 adb.devices = () => {
-  return new Promise((resolve, reject) => {
-    uid(20).then(uid => {
-      adb.tasks[uid] = exec(`${cmd} devices`, (error, stdout, stderr) => {
-        if (error) reject(error)
-        resolve(stdout)
-        delete adb.tasks[uid]
-      })
+  return new Promise(async (resolve, reject) => {
+    const id = await uid(20)
+    adb.tasks[id] = exec(`${cmd} devices`, (error, stdout, stderr) => {
+      if (error) reject(error)
+      resolve(stdout)
+      delete adb.tasks[id]
     })
   })
 }
 
 adb.list = adb.devices // alias
 
-adb.kill = (uuid) => {
-  if (adb.tasks.hasOwnProperty(uuid)) adb.tasks[uuid].kill()
-  delete adb.tasks[uuid]
+adb.kill = (id) => {
+  if (adb.tasks.hasOwnProperty(id)) {
+    adb.tasks[id].kill()
+    delete adb.tasks[id]
+  }
 }
 
 module.exports = adb
