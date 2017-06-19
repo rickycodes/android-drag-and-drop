6d8eceed3a0f67511ce2e9d75e72753b01c959f6
Ricky Miller <ricky.miller@gmail.com>
2017-06-18 23:41:00 -0400

    Update js, add dragover animation, fix onClose

--- renderer.js
+++ renderer.js
@@ -1,13 +1,5 @@
-console.log(`
-  We are using Node.js ${process.versions.node},
-  Chromium ${process.versions.chrome},
-  and Electron ${process.versions.electron}.
-`)
-
 const adb = window.adb = require('./adb')
-
 const qs = (selector) => document.querySelector(selector)
-
 const status = qs('.status')
 const holder = qs('.holder')
 
@@ -30,13 +22,11 @@ const poll = () => {
 
       connected
         ? holder.classList.add('ready')
-        : holder.classList.remove('ready')
+        : holder.classList.remove('ready', 'active')
 
       let statusText = `device(s) ${
         connected ? '' : 'dis'
-      }connected ${
-        connected ? '👍' : '💩'
-      }`
+      }connected`
 
       if (Object.keys(adb.tasks).length) {
         statusText = 'transfering files...'
@@ -50,51 +40,66 @@ const poll = () => {
 
 poll()
 
-holder.ondragover = () => false
-holder.ondragleave = holder.ondragend = () => false
+const createCard = () => {
+  const card = document.createElement('div')
+  card.setAttribute('class', 'card')
+  return card
+}
+
+const push = async (f, dest) => {
+  const card = createCard()
+  holder.appendChild(card)
+  const uid = await adb.push({
+    args: [f.path, dest],
+    ondata: (data) => {
+      const output = data.toString()
+      const percent = output.substring(
+        output.indexOf('(') + 1, output.indexOf('%')
+      )
+      card.innerHTML = `
+        <span class='details'>
+          ${percent}% transfering: "<span class='file'>${f.name}</span>" to ${dest}
+        </span>
+        <progress value='${percent}' max='100'>${percent} %</progress>
+      `
+    },
+    onexit: (code) => {
+      console.log(`Child exited with code ${code}`)
+      holder.removeChild(card)
+      reset()
+    }
+  })
+
+  card.setAttribute('data-uid', uid)
+  card.addEventListener('click', (event) => {
+    event.preventDefault()
+    adb.kill(card.dataset.uid)
+  })
+}
+
+holder.ondragover = () => {
+  holder.classList.add('over')
+  return false
+}
+
+holder.ondragleave = holder.ondragend = () => {
+  holder.classList.remove('over')
+  return false
+}
+
 holder.ondrop = (e) => {
   e.preventDefault()
   if (!connected || holder.classList.contains('active')) {
     return false
   }
 
-  holder.classList.remove('ready')
+  holder.classList.remove('ready', 'over')
   holder.classList.add('active')
 
   const dest = '/sdcard/'
   for (let f of e.dataTransfer.files) {
     console.log('File(s) you dragged here: ', f.path)
-
-    const card = document.createElement('div')
-    card.setAttribute('class', 'card')
-    holder.appendChild(card)
-
-    adb.push({
-      args: [f.path, dest],
-      ondata: (data) => {
-        const output = data.toString()
-        const percent = output.substring(
-          output.indexOf('(') + 1, output.indexOf('%')
-        )
-        card.innerHTML = `
-          <span class='details'>
-            ${percent}% transfering: "<span class='file'>${f.name}</span>" to ${dest}
-          </span>
-          <progress value='${percent}' max='100'>${percent} %</progress>
-        `
-      },
-      onexit: (code) => {
-        console.log(`Child exited with code ${code}`)
-        holder.removeChild(card)
-        reset()
-      }
-    }).then(uid => {
-      card.onclick = (e) => {
-        console.log(`close ${uid}`)
-        e.preventDefault()
-        adb.kill(uid)
-      }
-    })
+    push(f, dest)
   }
   return false
 }
